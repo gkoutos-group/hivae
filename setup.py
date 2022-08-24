31ae89adcf981566cea3e56b20d78ed22f01a8f8
Andreas Karwath <a.karwath@bham.ac.uk>
2022-08-24 19:16:59 +0100

    clarified tf2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     license='MIT',
     author='Andreas Karwath',
     author_email='a.karwath@bham.ac.uk',
-    description='HIVAE (Handling incomplete heterogeneous data using VAEs. - by Nazabal, et al., DOI: 10.1016/j.patcog.2020.107501, 2020) - Extenstion of implementations as easy to use Python library',
+    description='HIVAE (Handling incomplete heterogeneous data using VAEs. - by Nazabal, et al., DOI: 10.1016/j.patcog.2020.107501, 2020) - Extenstion of implementations as easy to use Python library/tf2 version',
     packages=setuptools.find_packages(exclude=['examples']),
     install_requires=requirements,
     long_description=readme,
