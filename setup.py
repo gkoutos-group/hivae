108c3926a821870a86592614a9e85da70211b777
Andreas Karwath <a.karwath@bham.ac.uk>
2023-06-13 15:33:27 +0100

    minor bug fixed and logging correction

--- setup.py
+++ setup.py
@@ -6,17 +6,19 @@ with open("README.md", "r") as readme_file:
 requirements = [#'tensorflow', - issues with my miniforge3 - apple silicone - tensorflow setup
                 'tensorflow-probability',    
                 'pandas',
-                'sklearn',
+                'scikit-learn',
                     ]
     
 setuptools.setup(
     name='hivae2',
-    version='0.9',
+    version='0.12',
     url='https://github.com/gkoutos-group/hivae/',
     license='MIT',
     author='Andreas Karwath',
     author_email='a.karwath@bham.ac.uk',
-    description='HIVAE (Handling incomplete heterogeneous data using VAEs. - by Nazabal, et al., DOI: 10.1016/j.patcog.2020.107501, 2020) - Extenstion of implementations as easy to use Python library/tf2 version',
+    description="""HIVAE (Handling incomplete heterogeneous data using VAEs. - by Nazabal, et al., DOI: 10.1016/j.patcog.2020.107501, 2020)
+Extenstion of implementations as easy to use Python library/tf2 version (a.karwath)
+Further extension to procude imputation objects for sklearn API (a.karwath) """,
     packages=setuptools.find_packages(exclude=['examples']),
     install_requires=requirements,
     long_description=readme,
